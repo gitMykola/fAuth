52f1b4ec38fb3abd33f9b80fb2057e2ebad2b734
Mykola <mykola_borodyn@ecoengineer.in.ua>
2017-09-28 22:26:59 +0300

    web3.eth api

--- providers/EthereumProvider.js
+++ providers/EthereumProvider.js
@@ -1,20 +1,33 @@
-let Web3 = require('web3');
+let Web3 = require('web3'); // Stabile version web3@0.19.0 !!!!!!!!!!!!!!!!!!!!!!!!!!
+    //acc = require('web3-eth-accounts');
 
 module.exports = {
     new:(data, next)=> {
-       /* if (typeof web3 !== 'undefined') {
+        let web3;
+        if (typeof web3 !== 'undefined') {
             web3 = new Web3(web3.currentProvider);
         } else {
             // set the provider you want from Web3.providers
             web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
         }
-        if(!web3.isConnected())
-            console.log("not connected");
-        else
-            console.log("connected");*/
-        next({data:{address:'12345678',
-            currency: data.currency,
-            userId:data.userId,
-            pas:data.passfrase}});
+        //console.dir(web3.has ('isConnected'));
+        if(web3.isConnected !== 'undefined' && !web3.isConnected())
+        {
+            console.log("ETH NET not connected!");
+            next({data:null});
+        }
+        console.log("connected");
+        //web3.eth.getAccounts().then((err,acc)=>{if(err)console.dir(err);else console.dir(acc)});
+        //let account = new
+        //console.dir(web3.eth.getBalance('0x8ae4f5a1f71b52ccedcaa6df3c17dd4a5341ab7d'));
+        web3.eth.getBalance('0x8ae4f5a1f71b52ccedcaa6df3c17dd4a5341ab7d',(err,result)=>{console.dir(result)});
+        console.dir(web3.eth.blockNumber.toString());
+        next({
+             data: {
+                  address: '12345678',
+                  currency: data.currency,
+                  userId: data.userId
+                  }
+             });
     },
 };
--- public/javascripts/operations.js
+++ public/javascripts/operations.js
@@ -6,14 +6,13 @@ function createAccount(e){
     let host = 'http://localhost:3000/api/v1.0/createAccount';
     let xhttp = new XMLHttpRequest();
     let data = {userId:'59cbd171dc2652236b1bbc29',
-        passfrase:'123456',
-        currency:'ETH'};
+                currency:'ETH'};
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             //console.dir(this.responseText);
             data = JSON.parse(this.responseText);
             console.dir(data);
-            document.querySelector('#request').innerHTML += data.toString();
+            document.querySelector('#request').innerHTML = data.toString();
         }
     };
     xhttp.open("POST", host, true);
