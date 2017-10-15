7c2e0ec8780d90ce52ab87f20b3e732aef001f31
Mykola <mykola_borodyn@ecoengineer.in.ua>
2017-10-15 19:07:14 +0300

    api_v.3.0 refactoring router commits

--- routes/api_3_0.js
+++ routes/api_3_0.js
@@ -23,52 +23,6 @@ router.post('/phonevalid',(req,res)=>{
         res.json(data);
     })
 });
-/*router.post('/phonevalid',(req,res)=>{
-    res.resData = {r:null,r1:{k1:null,u1:null}};
-    if(!User.validateData({phone:req.body.pn})) {
-        res.resData.r = 0;
-        res.json(res.resData);
-    }
-    else User.getUserByPhone(req.body.pn,(data)=>{
-        if(data.error){
-            res.resData.r = 'Serever database error!';
-            res.json(res.resData);
-        }else{
-            if(data.data){
-                res.resData.r = 1;
-                res.json(res.resData);
-            }else{
-                User.getTmpUserByPhone(req.body.pn,(tmpData)=> {
-                    if(tmpData.error){
-                        res.resData.r = 'Serever database error!';
-                        res.json(res.resData);
-                    }else {
-                        if(tmpData.data){
-                            res.resData.r = 1;
-                            res.json(res.resData);
-                        }else {
-                            let tmpUser = {
-                                name: 'PHONE USER',
-                                phone: req.body.pn,
-                                u1:rnd.generate(32),
-                            };
-                            User.setTempUser(tmpUser, (ms) => {
-                                if (ms.error) {
-                                    res.resData.r = 'Serever database error!';
-                                    res.json(res.resData);
-                                } else {
-                                    res.resData.r1.k1 = ms.data;
-                                    res.resData.r1.u1 = tmpUser.u1;
-                                    res.json(res.resData);
-                                }
-                            })
-                        }
-                    }
-                })
-            }
-        }
-    })
-});*/
 /*
 * OEIww
 * 59e22584159fe127acf1a8e7
@@ -87,63 +41,6 @@ router.post('/smsconfirm',(req,res)=>{
         res.json(data);
     })
 });
-/*router.post('/smsconfirm',(req,res)=>{
-    res.resData = {rs:null,rs1:{us1:null}};
-    if(!User.validateData({phone:req.body.pn})) {
-        res.resData.rs = 0;
-        res.json(res.resData);
-    }
-    else User.getUserByPhone(req.body.ph,(data)=> {
-        if (data.error) {
-            res.resData.rs = 'Serever database error!';
-            res.json(res.resData);
-        } else {
-            if (data.data) {
-                res.resData.rs = 1;
-                res.json(res.resData);
-            } else {
-                User.getTmpUserByPhone(req.body.ph,(tmpUser)=>{
-                    if(tmpUser.error){
-                        res.resData.rs = 'Serever database error!';
-                        res.json(res.resData);
-                    }else if(!tmpUser.data){
-                            res.resData.rs = 2;
-                            res.json(res.resData);
-                        }else if(req.body.ss !== tmpUser.data.message){
-                            res.resData.rs = 3;
-                            res.json(res.resData);
-                                }else if(req.body.s !== md5(tmpUser.data._id.toString())){
-                                            res.resData.rs = 4;
-                                            res.json(res.resData);
-                                        }else if((Date.now() - tmpUser.data.created_at) > config.app.tmpUserLive){
-                                    console.log((Date.now() - tmpUser.data.created_at));
-                                                    res.resData.rs = 5;
-                                                    res.json(res.resData);
-                                                }else User.setUser({
-                                                            name:'PHONE USER',
-                                                            phone:tmpUser.data.phone,
-                                                            created_at:Date.now(),
-                                                                    },(err,staticUser)=>{
-                                                        if(err){
-                                                            res.resData.rs = 'Server Database error!!';
-                                                            res.json(res.resData);
-                                                        }else{
-                                                            User.deleteTmpUser(tmpUser.data._id.toString(),(err)=>{
-                                                                if(err){
-                                                                    res.resData.rs = 'Server Database error!';
-                                                                    res.json(res.resData);
-                                                                }else{
-                                                                    res.resData.rs1.us1 = staticUser._id.toString();
-                                                                    res.json(res.resData);
-                                                                }
-                                                            })
-                                                        }
-                                                    });
-                })
-            }
-        }
-    })
-});*/
 /*
 * @body:{
 *           p001: - b870da30,
@@ -155,33 +52,6 @@ router.post('/password',(req,res)=>{
     Accounts.createAccountsViaPassword(req,res,(data)=>{
         res.json(data);
     })
-});
-router.post('/password',(req,res)=> {
-    res.resData = {rp: null, rp1: null};
-    if (!req.body.p001 || typeof(req.body.p001) !== 'string' || req.body.p001.length < 8) {
-        res.resData.rp = 0;
-        res.json(res.resData);
-    } else {
-        //console.log(req.query.sp);
-        //console.log(md5(req.body.sp));
-        if(req.query.sp !== req.body.sp){
-            res.resData.rp = 3;
-            res.json(res.resData);
-        } else {
-            Accounts.createForPhoneUser({
-                            userId:req.body.sp,
-                            web3:req.web3,
-                            pass:req.body.password,
-            },(acc)=>{
-                if(acc.error){
-                    res.resData.rp = acc.error;
-                    res.json(res.resData);
-                }else{
-                    res.json(res.resData);
-                }
-            });
-        }
-    }
 });
 router.post('/oauth2',(req,res)=>{});
 
